06fd37743ba90e040658474413141ffa38f5b94c
Quinton Fults <quinton.fults.work@gmail.com>
2021-09-16 11:56:43 -0700

    code

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const { join } = require('path')
+const passport = require('passport')
+const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
 
 const app = express()
 
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use()
 
 app.listen(process.env.PORT || 3000)
